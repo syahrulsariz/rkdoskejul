84ce14a3e849f29e8947ca4df13f1ac9ed099206
RKDO Service <75943517+syahrulsariz@users.noreply.github.com>
2025-09-22 20:50:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,19 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
+
 
 
 client.on('qr', qr => {
